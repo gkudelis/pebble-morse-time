c567d5b9913b478ff44833c3e77a3002b6a24fad
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2019-03-20 16:52:01 +0200

    Move the logic for setting up duration array into its own function

--- src/buzz.c
+++ src/buzz.c
@@ -6,6 +6,9 @@
 static Window *window;
 //static TextLayer *text_layer;
 
+// size of one time element
+const size_t element_size = sizeof(uint32_t);
+// array of sequences represening vibration patterns for digits
 static const uint32_t const morse_digits[][9] = {
     { 3*DD, 1*DD, 3*DD, 1*DD, 3*DD, 1*DD, 3*DD, 1*DD, 3*DD },
     { 1*DD, 1*DD, 3*DD, 1*DD, 3*DD, 1*DD, 3*DD, 1*DD, 3*DD },
@@ -19,34 +22,35 @@ static const uint32_t const morse_digits[][9] = {
     { 3*DD, 1*DD, 3*DD, 1*DD, 3*DD, 1*DD, 3*DD, 1*DD, 1*DD },
 };
 
-static uint32_t *segments;
+static uint32_t *durations;
+
+static void fill_durations_array(uint32_t *durations, char *time) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, time);
+    // read time string and use the numerical characters
+    memcpy(&durations[0], &morse_digits[0][0], 9 * element_size);
+    durations[9] = 3 * DD;
+    memcpy(&durations[10], &morse_digits[7][0], 9 * element_size);
+    durations[19] = 7 * DD;
+    memcpy(&durations[20], &morse_digits[2][0], 9 * element_size);
+    durations[29] = 3 * DD;
+    memcpy(&durations[30], &morse_digits[4][0], 9 * element_size);
+}
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "Select button pushed\n");
-
-    const size_t element_size = sizeof(uint32_t);
-
-    memcpy(&segments[0], &morse_digits[0][0], 9 * element_size);
-    segments[9] = 3 * DD;
-    memcpy(&segments[10], &morse_digits[7][0], 9 * element_size);
-    segments[19] = 7 * DD;
-    memcpy(&segments[20], &morse_digits[2][0], 9 * element_size);
-    segments[29] = 3 * DD;
-    memcpy(&segments[30], &morse_digits[4][0], 9 * element_size);
-
+    fill_durations_array(durations, "07:24");
     VibePattern pat = {
-        .durations = segments,
+        .durations = durations,
         .num_segments = 39,
     };
     vibes_enqueue_custom_pattern(pat);
 }
 
 static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+    window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
 }
 
 static void init(void) {
-    segments = (uint32_t*) malloc((4*9 + 3) * sizeof(uint32_t));
+    durations = (uint32_t*) malloc((4*9 + 3) * sizeof(uint32_t));
     window = window_create();
     window_set_click_config_provider(window, click_config_provider);
     const bool animated = false;
@@ -55,7 +59,7 @@ static void init(void) {
 
 static void deinit(void) {
     window_destroy(window);
-    free(segments);
+    free(durations);
 }
 
 int main(void) {
